76b145cddb7aca23dfcef24318b07cecb488336f
tananine <tana9dev@gmail.com>
2023-04-06 21:34:37 +0700

    add activate function controller

--- controllers/device.controllers.js
+++ controllers/device.controllers.js
@@ -6,6 +6,7 @@ const db = require('../models/index');
 const getRedirect = async (req, res, next) => {
   const scannerId = req.params.scannerId;
   const key = req.params.key;
+  let setAccountAction = false
 
   try {
     const scannerData = await db.Scanner.findByPk(scannerId, {
@@ -22,6 +23,11 @@ const getRedirect = async (req, res, next) => {
           key: key,
         });
       }
+      if (!scanner.Device.accountId) {
+        setAccountAction = true
+        return
+      }
+
       const accountId = scanner.Device.accountId;
       const profileType = scanner.type;
 
@@ -30,12 +36,51 @@ const getRedirect = async (req, res, next) => {
       }
       return getPrimaryLink(accountId);
     });
+    if (setAccountAction) {
+      return res.status(200).json({ setAccountAction: true, message: 'ตั้งค่า Device เชื่อม Account' });
+    }
     return res.status(200).json({ linkId: scannerData });
   } catch (error) {
     next(error);
   }
 };
 
+const activate = (req, res, next) => {
+  const accountId = req.account.id;
+  const scannerId = req.body.scannerId;
+  const key = req.body.key;
+
+  db.Scanner.findByPk(scannerId, {
+    include: { model: db.Device },
+  }).then(scanner => {
+    if (!scanner) {
+      throwError(404, 'ไม่พบ scanner นี้', {
+        scannerId: scannerId,
+      });
+    }
+    if (scanner.key !== key) {
+      throwError(404, 'Key ไม่ถูกต้อง', {
+        scannerId: scannerId,
+        key: key,
+      });
+    }
+
+    return db.Device.update({ accountId: accountId }, { where: { id: scanner.deviceId } })
+  }).then(update => {
+    if (update[0]) {
+      return res.status(200).json('อัพเดทสำเร็จ');
+    }
+    throwError(404, 'Update ไม่สำเร็จ', {
+      accountId: accountId,
+      scannerId: scannerId,
+      key: key,
+    });
+  }).catch(error => {
+    next(error)
+  })
+}
+
 module.exports = {
   getRedirect,
+  activate
 };
--- routes/device.routes.js
+++ routes/device.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const deviceControllers = require('../controllers/device.controllers');
+const authen = require('../middleware/authen');
 
 const router = express.Router();
 
 router.get('/scan/:scannerId/:key', deviceControllers.getRedirect);
+router.post('/activate', authen, deviceControllers.activate);
 
 module.exports = router;
